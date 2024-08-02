e06760361eff0d97053905968232d17d326719ef
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-02 18:07:56 +0530

    implemented register form

--- projects/register-form-project/components/auth/register-form.tsx
+++ projects/register-form-project/components/auth/register-form.tsx
@@ -11,9 +11,36 @@ import { Form,
   FormLabel
  } from "@/components/ui/form"
 
+ import { RegisterSchema } from "@/schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+ import {useForm} from "react-hook-form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { z } from "zod"
+ 
+ 
+
+
 
 
 const RegisterForm = () => {
+//creating a form using 'react-hook-form'
+const form = useForm({
+  resolver: zodResolver(RegisterSchema),
+  defaultValues: {
+     email: "",
+     name: "",
+     password: "",
+     confirmPassword: "",
+  }
+
+});
+
+const onSubmit = (data: z.infer<typeof RegisterSchema>) => {
+  console.log(data);
+}
+
+
   return (
     <CardWrapper
     label = "Create an Account"
@@ -21,9 +48,91 @@ const RegisterForm = () => {
     backButtonHref = "/auth/login"
     backButtonLabel = "Already have an account?. Login here"
     >
-      <div>
+    <Form {...form}>
+      {/* note the callback function onSubmit within the handleSubmit metho will be triggered only when the form is successfully validated*/}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="space-y-4">
         
-      </div>
+        {/* the FormField component takes in 3 props control, render and name
+        control is used to access the form methods
+         <!--
+          * 3 props for FormField component
+          * 1. control: this is used to access the form methods
+            this is responsible for controlling the form state and validation.
+          * 2. render: this is a callback function that takes in the formMethods and the provided name and returns the JSX for the field
+          * 3. name: this is used to identify the field and access its values in the form context
+          -->
+         
+
+        */}
+          <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} type="email" placeholder="Enter your email"/>
+              </FormControl>
+               {/* <!-- FormMessage is used to display error messages -->  */}
+              <FormMessage />
+            </FormItem>
+            
+          )}
+          
+          />
+          <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field}  placeholder="Enter your name"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+            
+          )}
+          
+          />
+          <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} type="password"  placeholder="Enter your password"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+            
+          )}
+          
+          />
+          <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input {...field} type="password"  placeholder="Enter password again"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+            
+          )}
+          
+          />
+
+        </div>
+        <Button className="w-full type=submit">Register</Button>
+      
+      </form>
+
+    </Form>
 
     </CardWrapper>
   )
